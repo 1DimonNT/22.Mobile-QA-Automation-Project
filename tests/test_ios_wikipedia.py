d7435304a288178b65cd9f1fd5789c2968b2be9d
1DimonNT <1DimonNT@rambler.ru>
2026-05-20 19:30:22 +0500

    fix: update requirements and fix ios test syntax

--- tests/test_ios_wikipedia.py
+++ tests/test_ios_wikipedia.py
@@ -9,10 +9,9 @@ from pages.wikipedia_app import wikipedia
 @allure.suite("Wikipedia Mobile Tests - iOS")
 @allure.tag("ios", "search", "browserstack")
 @allure.title("iOS: Wikipedia search should work")
+@pytest.mark.skip(reason="iOS requires separate app upload and configuration")
 class TestIOSWikipedia:
 
-    @pytest.mark.skip(reason="iOS requires separate app upload and configuration")
-    def test_ios_search_in_wikipedia__valid_query__should_find_results(self):
     @allure.title("iOS: Search for 'BrowserStack' on iPhone")
     @allure.severity(allure.severity_level.CRITICAL)
     @pytest.mark.ios
@@ -23,14 +22,14 @@ class TestIOSWikipedia:
         If app is not uploaded, the test will fail with capability error.
         """
         # Given: Wikipedia iOS app is open
-        wikipedia.open()
+        wikipedia.close_onboarding()
 
         # When: User searches for "BrowserStack"
         # Note: iOS selectors might differ from Android
         wikipedia.search("BrowserStack")
 
         # Then: Search results should be displayed
-        wikipedia.results_should_have_count_greater_than(0)
+        wikipedia.results_should_contain_text("BrowserStack")
 
         allure.attach(
             "iOS test executed on BrowserStack\n"
